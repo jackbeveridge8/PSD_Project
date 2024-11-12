6cf03223f74403f121265b0fc899034a366239ca
jackbeveridge8 <83995741+jackbeveridge8@users.noreply.github.com>
2024-11-12 21:48:49 +1100

    Update main.cpp

--- PlatformControllerUnit/PSD_GUI/main.cpp
+++ PlatformControllerUnit/PSD_GUI/main.cpp
@@ -1,9 +1,16 @@
 #include "mainwindow.h"
 #include <QApplication>
+#include <QDir>
+#include <QCoreApplication>
+#include <QDebug>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+
+    QDir::setCurrent(QCoreApplication::applicationDirPath());  // Ensure working directory is set to executable location
+    qDebug() << "Current working directory:" << QDir::currentPath();
+
     MainWindow w;
     w.show();
     return a.exec();
